c730e9c5e366035c9b7788754ae06cbe1a0bdc46
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-07-26 16:17:32 +0500

    optimisation arraysquare function

--- interview/arraysquare.js
+++ interview/arraysquare.js
@@ -1,6 +1,5 @@
 var sortedSquares = function(nums) {
-    nums = nums.map(num => num*num)
-   return nums.sort((a, b) => a - b)
+    return nums.map(num => num*num).sort((a, b) => a - b)
 };
 
 console.log(sortedSquares([-7,-3,2,3,11]))
