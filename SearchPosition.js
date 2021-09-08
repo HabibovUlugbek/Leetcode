d55ed11aa1ae9c6290cc566d625c895b5dceacfb
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-08 21:59:17 +0500

    binary search  problems solution

--- SearchPosition.js
+++ SearchPosition.js
@@ -6,3 +6,12 @@ var searchInsert = function(nums, target) {
 };
 
 console.log(searchInsert([1,3,5,6],5)
+
+// var searchInsert = function(nums, target) {
+    
+//     for(var i=0 ; i< nums.length ; i++){
+//         if(nums[i] >= target ) return i
+//     } 
+    
+//     return nums.length
+// };
---
+++ binarysearch.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+
+    return nums.indexOf(target)
+    };
