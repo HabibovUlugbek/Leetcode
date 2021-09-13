62eddb0d427ac9f599033cc1695d266a26c510c5
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-13 22:57:52 +0500

    new solution for rotate array

--- rotatearray.js
+++ rotatearray.js
@@ -8,3 +8,21 @@ var rotate = function(nums, k) {
 };
 console.log(rotate(
     [1,2,3,4,5,6,7], 3))
+
+    var rotate = function(nums, k) {
+   k %= nums.length // if k is greater than nums.length then one cycle is completed that means it will remain the same and we have to remainder shifts
+    
+   let reverse = function(i, j){
+    while(i < j){
+        let temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+        i++
+        j--
+    }
+   } // suppose  ----->---> 
+	reverse(0, nums.length-1); // reverse   <--<------
+	 reverse(0, k-1) // reverse first part ---><----
+   reverse(k, nums.length-1)// reverse second part --->----->
+    
+};
