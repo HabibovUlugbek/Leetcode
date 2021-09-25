808c45037d5bc40d2542e4b51a435e1820cdde05
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-09-25 23:17:51 +0500

    added 2 solution

---
+++ intersectionarraytwo.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+ var intersection = function(nums1, nums2) {
+    var arr = [];
+         for(var i =0 ; i< nums1.length;i++){
+             for(var j=0 ; j< nums2.length ; j++){
+                 if(nums1[i]===nums2[j] && !arr.includes(nums1[i])){
+                     arr.push(nums1[i])
+                     nums1.splice(i,1);
+                     nums2.splice(j,1)
+                     i--;
+                     j--;
+                     
+                 } 
+             }
+         }
+     return arr;
+     
+ };
