2e5531f36d6150f57ca0b3a4b2e9b4a0206b8c1a
Habibov Ulug'bek <habibovulugbek22@gmail.com>
2021-10-28 16:58:33 +0500

    added frequnt element problem solved

---
+++ kfrequentnum.js
@@ -0,0 +1,13 @@
+var topKFrequent = function(nums, k) {
+    
+    let map = {};
+    
+    for(let num of nums){
+        
+        map[num] = (map[num] || 0) + 1;
+        
+    }
+
+    return Object.entries(map).sort((a,b) => b[1] - a[1]).map(val => Number(val[0])).slice(0, k);
+    
+};
