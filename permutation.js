80752522324ad66524a16f714f02e8ca27374edf
HabibovUlugbek <habibovulugbek22@gmail.com>
2021-12-05 17:10:41 +0500

    added 2 solution about permitation

---
+++ permutation.js
@@ -0,0 +1,20 @@
+var permute = function(nums) {
+    const len = nums.length;
+    const res=[]
+    recursive(nums,0,len-1);
+    return res;
+    function recursive(num,start,end){
+        if(start===end){
+            res.push([...num])
+        }else{
+            for(let i=start;i<=end;i++){
+                
+                [nums[start],nums[i]] = [nums[i],nums[start]];
+                
+                recursive(num,start+1,end);
+                
+                [nums[start],nums[i]] = [nums[i],nums[start]];
+            }
+        }
+    }
+};
