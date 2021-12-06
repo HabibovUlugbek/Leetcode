916b404894f6b162f37e722afc6d1b310bf97918
HabibovUlugbek <habibovulugbek22@gmail.com>
2021-12-06 23:22:14 +0500

    added 3 solution

---
+++ floodfill.js
@@ -0,0 +1,16 @@
+var floodFill = function(image, sr, sc, newColor) {
+    recursive(image, sr, sc, image[sr][sc], newColor);
+    return image;
+};
+
+var recursive = (image, x, y, oldColor, newColor) => {
+    if (x < 0 || y < 0 || x >= image.length || y >= image[x].length ||
+       image[x][y] === newColor || image[x][y] !== oldColor) {
+        return;
+    }
+    image[x][y] = newColor;
+    recursive(image, x + 1, y, oldColor, newColor);
+    recursive(image, x, y + 1, oldColor, newColor);
+    recursive(image, x - 1, y, oldColor, newColor);
+    recursive(image, x, y - 1, oldColor, newColor);
+}
