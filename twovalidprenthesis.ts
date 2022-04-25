9230744762b8ba312d14964bfccbbac26a0c09b8
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-04-26 00:26:14 +0500

    Create twovalidprenthesis.ts

---
+++ twovalidprenthesis.ts
@@ -0,0 +1,7 @@
+function maxDepthAfterSplit(seq: string): number[] {
+    let arr = []
+    for(let i=0; i<seq.length; i++){
+        arr.push(seq[i] == "(" ? i & 1 : 1-i & 1)
+    }
+    return arr
+};
