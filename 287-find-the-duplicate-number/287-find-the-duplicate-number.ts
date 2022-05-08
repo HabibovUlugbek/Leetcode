890af2c67c92770be226ce1efbfd0713482add3d
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-08 11:54:44 +0500

    Time: 172 ms (32.89%), Space: 58.5 MB (24.60%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.ts
@@ -0,0 +1,9 @@
+function findDuplicate(nums: number[]): number {
+let map = {};
+    
+    for(let i=0;i<nums.length;i++) {
+        if(map[nums[i]]) return nums[i];
+        
+        map[nums[i]] = true;
+    }
+};
