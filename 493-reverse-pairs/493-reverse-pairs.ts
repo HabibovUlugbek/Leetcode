6e43392708270ee2a31bce63862791c167750dfa
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-14 11:59:07 +0500

    Time: 296 ms (80.00%), Space: 85.7 MB (20.00%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.ts
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var reversePairs = function(nums) {
+  let numReversePairs = 0; 
+  helper(nums);
+  return numReversePairs;
+  
+  function helper(nums) {
+    if (nums.length <= 1) return nums;   
+    const length = nums.length;
+    const left = helper(nums.slice(0, Math.floor(length/2)));
+    const right = helper(nums.slice(Math.floor(length/2)));    
+    return merge(left, right);    
+  }
+  
+  function merge(left, right) {
+    const nums_sorted = [];
+    let leftIndex = 0;
+    let rightIndex = 0;  
+    while(leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] > 2 * right[rightIndex]) {
+        numReversePairs += (left.length - leftIndex);
+        rightIndex++;
+      } else {
+        leftIndex++;
+      }
+    }    
+    leftIndex = 0;
+    rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        nums_sorted.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        let cur = leftIndex;
+        nums_sorted.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+    while (leftIndex < left.length) {
+      nums_sorted.push(left[leftIndex]);
+      leftIndex++;
+    }
+    while (rightIndex < right.length) {
+      nums_sorted.push(right[rightIndex]);
+      rightIndex++;
+    }
+    return nums_sorted;
+  }
+  
+};
