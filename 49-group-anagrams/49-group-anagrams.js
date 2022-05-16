2b48c36173ce801ac8a7ffbae0058c47489dd035
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-16 14:07:53 +0500

    Time: 115 ms (95.08%), Space: 53 MB (74.21%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,7 +5,7 @@
 var groupAnagrams = function(strs) {
     let map ={};
     for(let i=0; i< strs.length; i++){
-        let key = strs[i].split("").sort((a, b) => a.localeCompare(b)).join("");
+        let key = strs[i].split("").sort().join("");
         if(map[key]) map[key].push(strs[i]);
         else map[key] = [strs[i]]
     }
