67f4983c807bf32fb6e39858d733e90b0460f77e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-16 13:48:40 +0500

    Time: 154 ms (9.09%), Space: 45.5 MB (24.24%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.ts
@@ -0,0 +1,13 @@
+function uniqueOccurrences(arr: number[]): boolean {
+let map = {};
+    for(let i=0; i<arr.length; i++){
+        if(map[arr[i]]) map[arr[i]]++;
+        else map[arr[i]] = 1;
+    }
+    for(let i=0; i<arr.length; i++){
+         for(let j=i+1; j<arr.length; j++){
+             if(map[arr[i]] === map[arr[j]] && arr[i]!== arr[j]) return false
+         }
+    }
+    return true;
+};
