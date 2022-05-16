46572439e9d18adb672b98477f8a8c748072c0d3
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-16 14:07:02 +0500

    Time: 328 ms (8.68%), Space: 56.8 MB (9.04%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map ={};
+    for(let i=0; i< strs.length; i++){
+        let key = strs[i].split("").sort((a, b) => a.localeCompare(b)).join("");
+        if(map[key]) map[key].push(strs[i]);
+        else map[key] = [strs[i]]
+    }
+    return Object.values(map)
+};
