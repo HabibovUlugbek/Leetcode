6f0e423f98aaa5c2ac352d225e04a7a76a1d4f0f
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-23 14:30:11 +0500

    Time: 72 ms (65.10%), Space: 41.8 MB (86.94%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,7 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m,nums1.length-m)
-    nums1.push(...nums2)
-    nums1.sort((a,b) => a-b)
+    
+    const nums1Copy = nums1.slice(0, m);
+    let counter = 0;
+    
+    while(counter < n){
+        if(nums1[m] === 0){
+            nums1[m++] = nums2[counter++];
+        }
+    }
+    return nums1.sort((a,b) => a-b)
 };
