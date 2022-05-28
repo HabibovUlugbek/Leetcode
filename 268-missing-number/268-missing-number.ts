6adf09722c6e21bf18157f12a32af9b22d011485
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-28 13:48:08 +0500

    Time: 149 ms (14.17%), Space: 45.1 MB (71.39%) - LeetHub

---
+++ 268-missing-number/268-missing-number.ts
@@ -0,0 +1,7 @@
+function missingNumber(nums: number[]): number {
+    nums.sort((a,b) => a-b);
+    for(let i=0 ; i<nums.length; i++){
+        if(i !== nums[i]) return i;
+    }
+    return nums[nums.length-1]+1
+};
