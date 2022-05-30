f747fa428c0510d7b3e92b7a6ac41c25d82ad402
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-30 19:37:55 +0500

    Time: 115 ms (25.00%), Space: 45.8 MB (50.00%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.ts
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.ts
@@ -1,10 +1,7 @@
 function repeatedNTimes(nums: number[]): number {
-let hashtable = {};
+    let hashtable = {};
     for(let i=0 ;i<nums.length; i++){
-        if(hashtable[nums[i]]) hashtable[nums[i]]++
+        if(hashtable[nums[i]])  return nums[i];
         else hashtable[nums[i]] = 1 ;
-    }
-    for(let i=0 ;i<nums.length; i++){
-        if(hashtable[nums[i]] === nums.length/2) return nums[i] 
     }
 };
