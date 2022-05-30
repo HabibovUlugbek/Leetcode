5fe6e1d63da3644009cd566f7c69e679b0ad4a07
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-30 18:50:55 +0500

    Time: 116 ms (46.46%), Space: 44 MB (69.75%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    var num;
+    const MIN = Math.pow(-2, 31);
+    const MAX = MIN * -1 - 1;
+    num = parseInt(dividend/divisor);
+    if (num < MIN) {
+        return MIN;
+    } else if (num > MAX) {
+        return MAX;
+    }
+         else{
+            return num;
+        }
+    
+};
