d61003e5011ef7f63e5b00e907dcb8fca753da70
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-05-31 22:32:08 +0500

    Time: 726 ms (16.59%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,5 +1,3 @@
-SELECT
-    (SELECT DISTINCT Salary
-        FROM  Employee
-        ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1) AS SecondHighestSalary ;
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary NOT IN(SELECT MAX(Salary) from Employee)
