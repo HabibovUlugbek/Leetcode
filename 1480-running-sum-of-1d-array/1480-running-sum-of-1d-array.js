aec4563191e92d1040abb9c8b6ff72bc59534f1e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-06-01 18:31:32 +0500

    Time: 100 ms (23.51%), Space: 43 MB (10.42%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if(i===0){
+            nums[i]=nums[i]
+        }else {
+            nums[i]=nums[i]+nums[i-1];
+        } 
+       
+    }
+    return nums;
+};
