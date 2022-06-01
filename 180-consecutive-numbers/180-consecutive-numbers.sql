240aed9dae90ec0ddff8e59d8817a1bdf70a3cec
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-06-01 18:46:21 +0500

    Time: 1135 ms (15.58%), Space: 0B (100.00%) - LeetHub

---
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT(num) AS ConsecutiveNums 
+FROM Logs
+WHERE ((id+1, num) IN (SELECT id,num 
+                       FROM Logs) 
+                            AND
+(id+2, num) in (SELECT id, num 
+                FROM Logs));
