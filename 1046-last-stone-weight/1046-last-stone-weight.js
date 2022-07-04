55e46897a954b7240f2c215603f5194f5b576a36
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-07-04 20:08:22 +0500

    Time: 109 ms (26.74%), Space: 43.5 MB (49.06%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    stones = stones.sort((a,b) => b-a)
+    while(stones.length >= 2){
+          if(stones[0] === stones[1]) {
+            stones.shift();
+              stones.shift();
+              }
+        if(stones[0] > stones[1]){
+            x = stones[0] -stones[1]
+            stones.shift();
+            stones[0] = x;
+        }
+        
+        stones = stones.sort((a,b) => b-a)
+        
+          }
+    if(stones.length > 0){
+        return stones[0] 
+    } else  return 0;
+};
