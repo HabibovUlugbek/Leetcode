901aceb2f097c438f52f18008f1e5ea2c08aa247
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-08-01 10:51:04 +0500

    Time: 103 ms (58.00%), Space: 44.1 MB (54.00%) - LeetHub

---
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} queries
+ * @param {number} m
+ * @return {number[]}
+ */
+var processQueries = function(queries, m) {
+    let result = [];
+    let permutation = [];
+    for(let i=0; i<m; i++){
+        permutation.push(i+1);
+    }
+    
+    for(let i=0; i<queries.length; i++){
+        let index = permutation.indexOf(queries[i]);
+        result.push(index);
+        permutation.splice(index,1);
+        permutation.unshift(queries[i]);
+    }
+    return result;
+};
