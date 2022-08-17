bb9d667bf729cb9eab7e2406cbd40f2f71e4e828
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-08-17 10:13:59 +0500

    Time: 79 ms (78.83%), Space: 43.1 MB (68.25%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+   const morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."],res=[];
+    
+   words.forEach((word)=>{
+       let str=''
+       for(let i=0;i<word.length;i++){
+           str+= morse[word[i].charCodeAt()-97]
+       }
+       res.push(str);
+   })
+    return [...new Set(res)].length;
+};
