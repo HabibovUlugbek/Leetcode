da133261e891d120621b94539ab48f008928c85e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-08-29 16:58:21 +0500

    Time: 120 ms (63.39%), Space: 44.4 MB (94.85%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+	let count = 0;
+	
+	function callDFS(grid, i, j) {
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+			return;
+		}
+		
+		grid[i][j] = '0';
+		
+		callDFS(grid, i + 1, j); // down
+		callDFS(grid, i - 1, j); // up
+		callDFS(grid, i, j + 1); // right
+		callDFS(grid, i, j - 1); // left
+	};
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j] == '1') {
+				count += 1;
+				callDFS(grid, i, j);
+			}
+		}
+	}
+	
+	return count;
+};
