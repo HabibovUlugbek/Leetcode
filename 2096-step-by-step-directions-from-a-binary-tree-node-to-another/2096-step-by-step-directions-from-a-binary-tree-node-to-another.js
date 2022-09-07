41f91bd3bda3f42b55cbdb439b6389b37c91433c
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-07 12:34:40 +0500

    Time: 297 ms (95.21%), Space: 112.1 MB (51.93%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} startValue
+ * @param {number} destValue
+ * @return {string}
+ */
+var getDirections = function(root, startValue, destValue) {
+    const getPath = (node, value, acc='') => {
+        if (node === null) {
+            return '';
+        } else if (node.val === value) {
+            return acc;
+        } else {
+            return getPath(node.left, value, acc + 'L') + getPath(node.right, value, acc + 'R')
+        }
+    }
+    
+	// generate the paths
+    let startPath = getPath(root, startValue);
+    let destPath = getPath(root, destValue);
+    
+    // find the lowest common ancestor
+    let i = 0;
+    for (; i < startPath.length && i < destPath.length && startPath[i] === destPath[i]; i++);
+    
+	// output the final path
+    let output = '';
+    for (let j = i; j < startPath.length; j++) {
+        output += 'U';
+    }
+    
+    return output + destPath.substring(i);
+};
