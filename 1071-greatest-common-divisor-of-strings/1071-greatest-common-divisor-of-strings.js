016874628d3267895ad2d2b927ab95fa660b3ec3
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-16 15:38:02 +0500

    Time: 73 ms (83.25%), Space: 42 MB (94.09%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+const gcdOfStrings = (str1, str2) => {
+  if (str1 + str2 !== str2 + str1) return '';
+  const gcd = (a, b) => (0 === b ? a : gcd(b, a % b));
+  return str1.substring(0, gcd(str1.length, str2.length));
+};
