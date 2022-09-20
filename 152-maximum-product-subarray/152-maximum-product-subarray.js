7b2ca84c14bc036f87ddf5baf8af9e0dd957e445
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-20 15:21:54 +0500

    Time: 83 ms (76.58%), Space: 42.3 MB (81.69%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let prevMax = nums[0];
+    let prevMin = nums[0];
+    let result = nums[0];
+    for (let i=1;i<nums.length;i++) {
+        // given the new number, the new maximun can have 3 conditions
+        // 1. number(+) * prevMax(+) is the largest
+        // 2. number(+) it self is the largest
+        // 3. number(-) * prevMin(-) is the largest 
+        curMax = Math.max(nums[i] * prevMax, nums[i], nums[i] * prevMin);
+        
+        curMin = Math.min(nums[i] * prevMin, nums[i], nums[i] * prevMax);
+
+		// updating the prevMax & prevMin, these two may swap locations
+        prevMax = curMax
+        prevMin = curMin
+
+        result = Math.max(curMax, result);
+    }
+    return result;
+}
