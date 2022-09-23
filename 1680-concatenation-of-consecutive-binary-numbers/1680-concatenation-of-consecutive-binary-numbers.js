d8aaa2848b48ca40a2e71ee24c30ae2f9a84571e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-23 18:54:22 +0500

    Time: 198 ms (100.00%), Space: 43.8 MB (44.44%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -3,9 +3,13 @@
  * @return {number}
  */
 var concatenatedBinary = function(n) {
-    let num = 1;
-    for(let i=2; i<=n;i++){
-        num = (num*Math.pow(2,i.toString(2).length)+i)%(Math.pow(10,9)+7);
+    let str = 1
+    let len = 4
+    for (let i = 2; i <= n; i++) {
+        if (i === len) {
+            len = len * 2
+        }
+        str = (str * len + i) % 1000000007
     }
-    return num % (Math.pow(10,9)+7)
+    return str
 };
