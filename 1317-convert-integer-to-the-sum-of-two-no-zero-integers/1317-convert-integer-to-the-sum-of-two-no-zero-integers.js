3a56638c5bc9cfc621865b04bdbb19065fb8c2c8
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-23 21:56:36 +0500

    Time: 67 ms (90.38%), Space: 42.5 MB (39.42%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -4,7 +4,6 @@
  */
 var getNoZeroIntegers = function(n) {
     for(let i = 1; i<=n/2 ;i++){
-        console.log(i ,!i.toString().includes("0"))
         if(!i.toString().includes("0") && !(n-i).toString().includes("0"))  return [i,n-i]
     }
 };
