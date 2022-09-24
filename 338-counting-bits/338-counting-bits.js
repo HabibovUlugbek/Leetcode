c607e3a68b7593171d62b6abed77ae7f8a25d3d1
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-24 13:01:47 +0500

    Time: 95 ms (91.61%), Space: 47.5 MB (93.32%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -3,15 +3,12 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    let res = []
-    for(let i=0 ; i<=n ; i++){
-        let a = i;
-        let count = 0
-        while(a){
-            a &=(a-1);
-            count++
-        }
-        res.push(count)
+    let dp = new Array(n+1).fill(0);
+    let offset = 1;
+    dp[0] = 0;
+    for(let i=1 ; i<=n ; i++){
+        if(i === offset*2) offset =i;
+        dp[i] = 1+dp[i-offset]        
     }
-    return res;
+    return dp;
 };
