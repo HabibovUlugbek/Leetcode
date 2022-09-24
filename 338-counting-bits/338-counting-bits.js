f6a8a5b4471d4e718cb7c997dce1cab15f69b80d
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-24 13:02:51 +0500

    Time: 150 ms (47.09%), Space: 47.5 MB (95.09%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -5,7 +5,6 @@
 var countBits = function(n) {
     let dp = new Array(n+1).fill(0);
     let offset = 1;
-    dp[0] = 0;
     for(let i=1 ; i<=n ; i++){
         if(i === offset*2) offset =i;
         dp[i] = 1+dp[i-offset]        
