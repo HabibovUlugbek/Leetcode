a7316f54e53138bee1941aea40ecb2d79d26e1b3
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-09-27 09:30:30 +0500

    Time: 825 ms (37.31%), Space: 75.3 MB (25.37%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.js
+++ 838-push-dominoes/838-push-dominoes.js
@@ -2,24 +2,39 @@
  * @param {string} dominoes
  * @return {string}
  */
-var pushDominoes = function(domino) {
-    let dominoes = domino.split("");
-    let another = [...dominoes];
-    for(let i=0 ; i< dominoes.length ; i++) {
-        if(dominoes[i] === "."){
-            // console.log(dominoes.join(""))
-            // console.log(another.join(""))
+var pushDominoes = function(dominoes) {
+    
+    
+    let workStr = dominoes.split("");
+    let lastRes = [...dominoes];
+    let finished = false;
+    
+    while (!finished) {
+        for (let i = 0; i < workStr.length; i++) {
+            const v = workStr[i];
+            if (v === 'L' || v === 'R') continue;
+            const nL = (i - 1) >= 0 ? lastRes[i-1] : null;
+            const nR = (i + 1) <= workStr.length ? lastRes[i+1] : null;
+            const stat = nL === 'R' && nR === 'L';
+            if (stat) continue;
+            if (!!nL && nL === 'R') {
+                workStr[i] = 'R';
+            } else if (!!nR && nR === 'L') {
+                workStr[i] = 'L';
+            }
+        }
             
-            if(dominoes[i-1] === "R" && dominoes[i+1] === "L") continue;
-            else if(dominoes[i-1] !== "R" && dominoes[i+1] === "L"){ 
-                another[i] = "L";
-            }
-            else if(dominoes[i-1] === "R" && dominoes[i+1] !== "L") {
-                another[i] = "R";
-            }
-        } else continue;
+        if ((workStr.length == lastRes.length) && (workStr.every((item, index) => {
+            return item === lastRes[index]
+        }))) {
+            finished = true;
+            break;
+        } else {
+            lastRes = [...workStr];
+        }
+        
     }
-    another = another.join("");
-    if(another === domino) return another;
-    return pushDominoes(another)
+    
+    
+    return workStr.join("");
 };
