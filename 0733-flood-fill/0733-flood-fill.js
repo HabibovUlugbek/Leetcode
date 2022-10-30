4fd62b98bb8e8262d23a965d76de03a38d569225
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-10-30 16:42:21 +0500

    Time: 111 ms (71.63%), Space: 44.1 MB (77.20%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+const floodFill = (image, sr, sc, newColor, firstColor = image[sr][sc]) => {
+    // handle if the coordinate is out of bounds
+	// or if it is already the new color
+	// or if it's not from the original color we're trying to change
+    if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[sr].length || image[sr][sc] !== firstColor || image[sr][sc] === newColor)   {
+        return image; // return image as-is
+    }
+    
+    image[sr][sc] = newColor;
+    
+    floodFill(image, sr + 1, sc, newColor, firstColor);
+    floodFill(image, sr - 1, sc, newColor, firstColor);
+    floodFill(image, sr, sc + 1, newColor, firstColor);
+    floodFill(image, sr, sc - 1, newColor, firstColor);
+    
+	// return modified image
+    return image;
+};
