ab17aea37075e28d6d26efe33e53ce1e6a4d7b00
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2022-10-31 10:53:18 +0500

    Time: 116 ms (43.23%), Space: 44 MB (63.53%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.js
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -3,16 +3,9 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    for(let i=0 ; i<matrix.length; i++){
-        for(let j=0; j<matrix[i].length; j++){
-            let num = matrix[i][j];
-            let k=i;
-            let l=j;
-            while(k < matrix.length && l< matrix[k].length){
-                if(num !== matrix[k][l]) return false;
-                k++;
-                l++;
-            }
+    for(let i=1 ; i<matrix.length; i++){
+        for(let j=1; j<matrix[i].length; j++){
+            if(matrix[i-1][j-1] !== matrix[i][j]) return false;
         }
     }
     
