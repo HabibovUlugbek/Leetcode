18bc6d30e41dbf62ded694a3ea16a8e7feef06f0
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-02-11 12:49:28 +0500

    Time: 94 ms (63.22%), Space: 46.9 MB (75.86%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.js
@@ -0,0 +1,47 @@
+var shortestAlternatingPaths = function (n, red_edges, blue_edges) {
+  // Generate Graph
+  const graph = {};
+  for (let i = 0; i < n; i++) graph[i] = { r: [], b: [] };
+  red_edges.forEach(([i, j]) => graph[i].r.push(j));
+  blue_edges.forEach(([i, j]) => graph[i].b.push(j));
+
+  const res = Array(n).fill(-1);
+  const visited = {
+    r: new Set(),
+    b: new Set(),
+  };
+
+  const queue = [
+    [0, 'r'],
+    [0, 'b'],
+  ];
+
+  let len = 0;
+
+  while (queue.length) {
+    const j = queue.length;
+
+    for (let i = 0; i < j; i++) {
+      const [currValue, currColor] = queue.shift();
+
+      // First time reaching this node
+      if (res[currValue] === -1) {
+        res[currValue] = len; // Update node with current len of paths
+      }
+
+      const nextValues = graph[currValue][currColor];
+      const nextColor = currColor === 'b' ? 'r' : 'b';
+
+      nextValues.forEach((nextValue) => {
+        // If next node is not visited, update visited and push to queue
+        if (!visited[nextColor].has(nextValue)) {
+          visited[nextColor].add(nextValue);
+          queue.push([nextValue, nextColor]);
+        }
+      });
+    }
+    len++;
+  }
+
+  return res;
+};
