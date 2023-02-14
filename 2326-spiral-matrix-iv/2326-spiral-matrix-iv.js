b63dba5f398d11a1894ea78ee4ed8b448e4d1fb4
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-02-14 11:16:47 +0500

    Time: 593 ms (75.56%), Space: 82.3 MB (53.33%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {ListNode} head
+ * @return {number[][]}
+ */
+var spiralMatrix = function(m, n, head) {
+    var matrix = new Array(m).fill().map(()=> new Array(n).fill(-1))
+    var row=0, col=0;
+    var direction="right";
+    while(head)
+        {
+            matrix[row][col]=head.val;
+            if(direction=="right")
+                {
+                    if(col+1 == n || matrix[row][col+1] != -1)
+                        {
+                            direction="down"
+                            row++;
+                        }
+                    else
+                        col++
+                }
+            else if(direction=="down")
+                {
+                    if(row+1 == m || matrix[row+1][col] != -1)
+                        {
+                            direction="left"
+                            col--;
+                        }
+                    else
+                        row++
+                }
+            else if(direction=="left")
+                {
+                    if(col == 0 || matrix[row][col-1] != -1)
+                        {
+                            direction="up"
+                            row--;
+                        }
+                    else
+                        col--
+                }
+            else if(direction=="up")
+                {
+                    if(row == 0 || matrix[row-1][col] != -1)
+                        {
+                            direction="right"
+                            col++;
+                        }
+                    else
+                        row--
+                }
+            head = head.next;
+        }
+    return matrix;
+};
