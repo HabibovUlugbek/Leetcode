a323113b8366b2c3606079ce88d65809ed90ab7a
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-02-21 12:30:58 +0500

    Time: 72 ms (59.35%), Space: 44.2 MB (42.03%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -6,7 +6,6 @@ var countGoodSubstrings = function(s) {
     let count=0;
     for(let i=0 ; i<s.length-2; i++){
         let str = s.slice(i,i+3);
-        console.log(str)
         if(new Set(...str.split()).size === 3) count++
     }
     return count
