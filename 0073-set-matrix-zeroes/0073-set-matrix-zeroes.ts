0f8ca7058d2a81195a575ddd4bf0ad83e3da5733
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-02-24 11:40:26 +0500

    Time: 91 ms (61.24%), Space: 45.3 MB (52.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -1,24 +1,24 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
 function setZeroes(matrix: number[][]): void {
-    const rows: Set<number> = new Set();
-    const cols: Set<number> = new Set();
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i][j] == 0) {
-                rows.add(i);
-                cols.add(j);
-            }
-        }
+  const row = new Set();
+  const col = new Set();
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        row.add(i);
+        col.add(j);
+      }
     }
+  }
 
-    for (let i of rows.values()) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            matrix[i][j] = 0;
-        }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (row.has(i) || col.has(j)) {
+        matrix[i][j] = 0;
+      }
     }
+  }
 
-    for (let j of cols.values()) {
-        for (let i = 0; i < matrix.length; i++) {
-            matrix[i][j] = 0;
-        }
-    }
 };
