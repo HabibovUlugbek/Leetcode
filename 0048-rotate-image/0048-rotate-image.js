2ef6332ab497cfbbfe6a629836645411e897a24f
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-02-24 16:06:16 +0500

    Time: 55 ms (95.31%), Space: 41.7 MB (82.95%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    var len =matrix.length;
+      for(var i=0;i<len;i++){
+          for(var j=i+1;j<len;j++){
+              var temp = matrix[i][j]
+              matrix[i][j] = matrix[j][i]
+              matrix[j][i] = temp
+          }
+      }
+      for(var i=0;i<matrix.length;i++){
+          matrix[i].reverse()
+      }  
+};
