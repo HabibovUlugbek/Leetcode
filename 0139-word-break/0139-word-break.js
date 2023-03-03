764088ce6149849f2ec014ca6e02baa16b2cf1ae
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-03-03 11:33:17 +0500

    Time: 71 ms (70.83%), Space: 43.7 MB (65.90%) - LeetHub

---
+++ 0139-word-break/0139-word-break.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = (s, wordDict) => {
+  if (wordDict == null || wordDict.length === 0) return false;
+
+  const set = new Set(wordDict);
+  const dp = Array(s.length + 1).fill(false);
+  dp[0] = true;
+
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      const w = s.slice(start, end);
+      if (dp[start] === true && set.has(w)) {
+        dp[end] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+};
