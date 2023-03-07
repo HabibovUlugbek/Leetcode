19ef1b1f48d7ac50f351246dbdf50480a78ed593
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-03-07 15:27:48 +0500

    Time: 70 ms (78.10%), Space: 44.1 MB (94.56%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -1,18 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var leftRigthDifference = function(nums) {
-    let len = nums.length
-    let left = new Array(len).fill(0);
-    let right = new Array(len).fill(0);
-    for(let i=0 ;i<nums.length; i++){
-        if(i-1 >= 0)  left[i] = left[i-1]+nums[i-1];
-        if(i !== 0) right[len-1-i] = right[len-i]+nums[len-i];
-    }    
-    let result =new Array(len).fill(0)
-    for(let i=0 ;i<len; i++){
-       result[i] = Math.abs(right[i]-left[i])
-    }
-    return result;
+     let leftSum = 0;
+    let rightSum = nums.reduce((sum,curr)=>sum += curr,0);
+
+    return nums.map((curr,i)=>{
+        //leftSum - (rightSum - curr)
+        rightSum -= curr;
+        let res = Math.abs(leftSum - rightSum );
+        leftSum += curr;
+        return res
+    });
 };
