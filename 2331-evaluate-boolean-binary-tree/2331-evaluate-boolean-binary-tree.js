5b638e9d2ea5dc2f640d09fcc65167a77f27950f
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-03-20 15:52:11 +0500

    Time: 80 ms (37.64%), Space: 49.1 MB (18.54%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -11,11 +11,7 @@
  * @return {boolean}
  */
 var evaluateTree = function(root) {
-    if(!root) return false;
-    if(root.val === 2) return evaluateTree(root.left) || evaluateTree(root.right)
-    if(root.val === 3) return  evaluateTree(root.left) && evaluateTree(root.right)
-    if(root.val === 1) return true;
-    if(root.val === 0) return false;
-    return evaluateTree(root)
-
+  return root.val === 3 ? evaluateTree(root.left) && evaluateTree(root.right) :
+         root.val === 2 ? evaluateTree(root.left) || evaluateTree(root.right) :
+         root.val
 };
