d8bc48ebfb3a2cce8a1af134ac99be0f6abd45d0
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-03 12:26:40 +0500

    Time: 182 ms (61.50%), Space: 66.8 MB (55.08%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
@@ -0,0 +1,7 @@
+var findSmallestSetOfVertices = function(n, edges) {
+    const degree = Array(n).fill(0);
+    const output = [];
+    edges.forEach(([u, v]) => degree[v]++);
+    degree.forEach((deg, i) => !deg && output.push(i));
+    return output;
+};
