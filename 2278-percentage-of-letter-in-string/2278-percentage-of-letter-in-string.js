46b300aebe573c774970cdc8755fd9b293abadfb
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-07 14:35:35 +0500

    Time: 70 ms (11.89%), Space: 41.9 MB (63.93%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+    let str = s.replaceAll(letter,"");
+    
+    return Math.floor((s.length-str.length)/s.length *100)
+};
