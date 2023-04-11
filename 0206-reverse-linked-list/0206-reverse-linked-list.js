cf8aa1c4103f0e09414f3077d61bf505e5396b18
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-11 17:52:27 +0500

    Time: 52 ms (97.44%), Space: 44.1 MB (61.53%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,14 @@
+var reverseList = function(head) {
+    let prev = null
+    let curr = head
+    let next = null
+    
+    while(curr!== null){
+        next = curr.next
+        // reverse
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev;
+};
