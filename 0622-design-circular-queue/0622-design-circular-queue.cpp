38ca612808467edf48b306a689210be9d40445f0
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-14 15:20:54 +0500

    Time: 28 ms (36.63%), Space: 16.8 MB (66.20%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,69 +1,34 @@
-/**
- * @param {number} k
- */
-var MyCircularQueue = function(k) {
-    this.queue= []
-    this.length = k;
+class MyCircularQueue {
+public:
+    MyCircularQueue(int k) {
+        data.resize(k);
+        maxSize = k;
+    }
+    bool enQueue(int val) {
+        if (isFull()) return false;
+        tail = (tail + 1) % maxSize;
+        data[tail] = val;
+        return true;
+    }
+    bool deQueue() {
+        if (isEmpty()) return false;
+        if (head == tail) head = 0, tail = -1;
+        else head = (head + 1) % maxSize;
+        return true;
+    }
+    int Front() {
+        return isEmpty() ? -1 : data[head];
+    }
+    int Rear() {
+        return isEmpty() ? -1 : data[tail];
+    }
+    bool isEmpty() {
+        return tail == -1;
+    }
+    bool isFull() {
+        return !isEmpty() && (tail + 1) % maxSize == head;
+    }
+private:
+    int maxSize, head = 0, tail = -1;
+    vector<int> data;
 };
-
-/** 
- * @param {number} value
- * @return {boolean}
- */
-MyCircularQueue.prototype.enQueue = function(value) {
-    if(this.isFull()) return false 
-    this.queue.push(value) 
-    return true;
-};
-
-/**
- * @return {boolean}
- */
-MyCircularQueue.prototype.deQueue = function() {
-    if(this.isEmpty()) return false
-     this.queue.shift();
-    return true;
-};
-
-/**
- * @return {number}
- */
-MyCircularQueue.prototype.Front = function() {
-    if(this.isEmpty()) return -1
-    return this.queue[0];
-};
-
-/**
- * @return {number}
- */
-MyCircularQueue.prototype.Rear = function() {
-     if(this.isEmpty()) return -1
-     return this.queue[this.queue.length-1];
-};
-
-/**
- * @return {boolean}
- */
-MyCircularQueue.prototype.isEmpty = function() {
-     if(this.queue.length === 0) return true
-     return false
-};
-
-/**
- * @return {boolean}
- */
-MyCircularQueue.prototype.isFull = function() {
-    if(this.queue.length === this.length) return true
-    return false
-};
-
-/** 
- * Your MyCircularQueue object will be instantiated and called as such:
- * var obj = new MyCircularQueue(k)
- * var param_1 = obj.enQueue(value)
- * var param_2 = obj.deQueue()
- * var param_3 = obj.Front()
- * var param_4 = obj.Rear()
- * var param_5 = obj.isEmpty()
- * var param_6 = obj.isFull()
- */
