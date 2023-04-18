8586b9638b1bfe2a4df2c604405d3125d8d86fd9
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-18 12:36:08 +0500

    Time: 73 ms (30.00%), Space: 45.9 MB (7.50%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.ts
@@ -0,0 +1,13 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let result = ""
+    
+    let i = 0
+    
+    while(i < word1.length || i < word2.length){
+        result += word1[i] !== undefined ? word1[i] : ""
+        result += word2[i] !== undefined ? word2[i] : ""
+        i++
+    }
+    
+    return result
+};
