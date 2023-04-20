09e84293484671f4ee2389b842cc43866cd22577
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-04-20 15:13:22 +0500

    Time: 67 ms (71.79%), Space: 44.8 MB (25.64%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.js
@@ -0,0 +1,32 @@
+var removeZeroSumSublists = function(head) {
+    if (!head) {
+        return head;
+    }
+    
+	// Initialize
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    
+    // Step1: build the prefix sum map
+    let curr = dummy 
+    let prefixSumMap = new Map(); 
+    let runningSum = 0; 
+    
+    while (curr) {
+        runningSum += curr.val;
+        prefixSumMap.set(runningSum, curr);
+        curr = curr.next;
+    }
+    
+    // Step 2: build the output
+    curr = dummy;
+    runningSum = 0;
+    
+    while (curr) {
+        runningSum += curr.val; 
+        curr.next = prefixSumMap.get(runningSum).next;
+        curr = curr.next; 
+    }
+    
+    return dummy.next;
+};
