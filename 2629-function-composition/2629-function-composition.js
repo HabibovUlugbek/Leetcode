5b51ac9823a06366bc54012670a1046b3428cad2
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-05-03 16:48:49 +0500

    Time: 74 ms (44.60%), Space: 43.4 MB (51.55%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+
+        for ( let i = functions.length; i--; ) {
+            x = functions[i](x);
+        }
+
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
