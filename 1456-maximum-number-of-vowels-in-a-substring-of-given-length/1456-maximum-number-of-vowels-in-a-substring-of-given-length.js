263b4fc3fbf358a4c47d027aafc0771f595f4f67
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-05-05 16:44:17 +0500

    Time: 107 ms (27.08%), Space: 45.1 MB (86.11%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -7,18 +7,13 @@ var maxVowels = function(s, k) {
   let vowel = ['a','i','u','e',"o"]
   let result= 0; 
   let count = 0;
-  
   for (let i = 0; i < k; i++) {
       if(vowel.includes(s[i]))++count
   }
-    // console.log(count)
   for (let j = 0; j <= s.length -k; j++) {
-      
       if(j !== 0) {
           if(vowel.includes(s[j-1]))--count
-          if(vowel.includes(s[j+k-1]))++count
-          // console.log(j,s[j-1],s[j+k-1], count )
-          
+          if(vowel.includes(s[j+k-1]))++count          
       }
       result = Math.max(result, count)
       if(result === k) return result
