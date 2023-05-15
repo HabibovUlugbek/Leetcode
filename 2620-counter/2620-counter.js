9cf3274d6e23ed9b12458e9ea59f815a61d316ee
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-05-15 15:07:53 +0500

    Time: 58 ms (51.39%), Space: 42.1 MB (43.40%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n-1
+    let result = []
+    return function() {
+        count++
+        return count
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
