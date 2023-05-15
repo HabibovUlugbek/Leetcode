5b77be3d083a4aff41e267e32a16f9c3266015fa
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-05-15 09:53:38 +0500

    Time: 4429 ms (6.45%), Space: 103 MB (19.35%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapNodes(head: ListNode | null, k: number): ListNode | null {
+    let arr:number[] = [];
+    let temp = head;
+    while(temp){
+        arr.push(temp.val);
+        temp = temp.next;
+    }
+    let change = arr[k-1];
+    arr[k-1] = arr[arr.length - k];
+    arr[arr.length - k] = change ; 
+    
+    temp = head;
+    while(temp){
+        temp.val = arr.shift();
+        temp = temp.next;
+    };
+    return head;
+};
