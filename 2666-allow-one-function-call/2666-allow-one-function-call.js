05d27b7f95d05f81bd5571c026a0fc929be25d7c
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-05-17 15:41:25 +0500

    Time: 52 ms (87.47%), Space: 41.8 MB (57.95%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let count = 0 ;
+    return function(...args){
+        if(count>=1) return undefined
+        else {
+            count++;
+            return fn(...args)
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
