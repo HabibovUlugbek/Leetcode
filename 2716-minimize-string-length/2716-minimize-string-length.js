8c4db56a8550edded40e787a55dccc5f09776ca4
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-07-24 11:22:12 +0500

    Time: 233 ms (7.24%), Space: 58.9 MB (8.68%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.js
+++ 2716-minimize-string-length/2716-minimize-string-length.js
@@ -5,8 +5,7 @@
 var minimizedStringLength = function(s) {
     let hash = {}
     for(let i=0 ; i<s.length; i++) {
-        if(hash[s[i]])hash[s[i]]++
-        else hash[s[i]] = 1
+        if(!hash[s[i]])hash[s[i]] = 1
     }
     
     return Object.keys(hash).length
