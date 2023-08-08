b1c03e2bef2a0b0e62c3aaf1250d4ba9d5f1c38c
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-08-08 15:32:30 +0500

    Time: 76 ms (93.89%), Space: 52.4 MB (75.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let hash = new Map()
+    let hash = new Set()
     for(let i=0 ; i<nums.length; i++){
         if(hash.has(nums[i]))return true 
-        else hash.set(nums[i], 1)
+        else hash.add(nums[i], 1)
     }
     return false
 };
