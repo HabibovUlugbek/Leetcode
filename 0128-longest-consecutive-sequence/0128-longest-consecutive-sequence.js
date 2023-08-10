983db4b60a4cee5df61f32e9640b990b645d6997
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-08-10 17:38:23 +0500

    Time: 185 ms (44.82%), Space: 76.3 MB (12.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,25 +3,27 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let max = 0;
-    let count = 0;
+    if(nums.length < 2 ) return nums.length
     let hash = {}
-    for(let i=0;i<nums.length ;i++){
-         if(hash[nums[i]])hash[nums[i]]++
-         else hash[nums[i]]=1;
+    for(let i=0;i<nums.length; i++){
+        if(hash[nums[i]]) continue
+        else hash[nums[i]]=1
     }
-    let values = Object.keys(hash).sort((a,b) => a-b)
-    if(values.length <2) return values.length
-    for(let i=0;i<values.length-1 ;i++){
-        if(values[i+1]-values[i] === 1) {
+    
+    const values = Object.keys(hash).sort((a,b) => a-b)
+    if(values.length===1) return 1
+    let max = 0
+    let count= 0
+    
+    for(let i=1; i<values.length; i++){
+        if(values[i]-values[i-1] === 1){
             if(count === 0) count++
-            count++;
-        }
-        else {
-            max = Math.max(count,max)            
+            count++
+        }else {
+            max= Math.max(count,max)
             count = 0
         }
     }
-    max = Math.max(count,max) 
-    return max === 0 ? 1 : max
+    max= Math.max(count,max)
+    return  max === 0 ? 1 : max;
 };
