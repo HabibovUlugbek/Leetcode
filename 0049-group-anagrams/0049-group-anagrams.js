2cee30ab2903bb128150469647a080dbb95d650e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-09-04 10:14:03 +0500

    Time: 106 ms (80.62%), Space: 52.4 MB (85.70%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -5,7 +5,7 @@
 var groupAnagrams = function(strs) {
     let hash = {};
     strs.forEach((str) => {
-        let decode = str.split("").sort()
+        let decode = str.split("").sort().join("")
         if(hash[decode]) hash[decode].push(str)
         else hash[decode] = [str]
     })
