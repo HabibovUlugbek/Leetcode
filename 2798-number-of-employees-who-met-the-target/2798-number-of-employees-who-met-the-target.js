4a0b329c07601ebb608861f6cab8ef5008476c04
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-11-02 11:54:15 +0500

    Time: 65 ms (19.69%), Space: 43.4 MB (85.08%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    const result = hours.filter(e => e >= target)
+    return result.length
+};
