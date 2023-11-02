79f1c8e729c6c88eec137f1bfff09a8deb576d3e
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-11-02 12:01:19 +0500

    Time: 63 ms (36.61%), Space: 43.4 MB (76.24%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    for(let i= 0 ; i<nums.length-1; i++){
+        for(let j=i+1; j<nums.length;j++){
+            if(nums[i]+nums[j] < target) count++
+        }
+    }
+        return count;
+};
