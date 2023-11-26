c617e0638d2beeb659814ae6f7a736f35e13f9fa
Habibov Ulug'bek <72121629+HabibovUlugbek@users.noreply.github.com>
2023-11-26 21:28:08 +0500

    Time: 57 ms (60.14%), Space: 42.8 MB (17.34%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let num1 = 0;
+    let num2 = 0;
+    
+    for(let i=1; i<=n ; i++){
+        if(i%m !== 0) num1+=i
+        else num2+=i
+    }
+    
+    return num1 - num2
+};
